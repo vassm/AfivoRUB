7454105980b4d7ff30e298502c6334a760b422c9
vassm <matefiamx3@gmail.com>
2026-01-19 18:19:04 +0100

    dielectric added, looking for high eps problem

--- programs/SDBD/m_user.f90
+++ programs/SDBD/m_user.f90
@@ -12,15 +12,28 @@ module m_user
 
   public :: user_initialize
 
-  ! Geometry parameters read from cfg
+  ! -------------------------
+  ! Electrode geometry
+  ! -------------------------
   real(dp) :: user_circle_radius = 1.0e-4_dp
-  real(dp) :: user_powered1_rel(NDIM)  = [0.40_dp, 0.70_dp]
-  real(dp) :: user_powered2_rel(NDIM)  = [0.60_dp, 0.70_dp]
-  real(dp) :: user_grounded1_rel(NDIM) = [0.40_dp, 0.30_dp]
-  real(dp) :: user_grounded2_rel(NDIM) = [0.60_dp, 0.30_dp]
 
-  ! Absolute positions (computed after reading cfg)
-  real(dp) :: powered1(NDIM), powered2(NDIM), grounded1(NDIM), grounded2(NDIM)
+  real(dp) :: user_powered1_rel(NDIM)  = [0.30_dp, 0.54_dp]
+  real(dp) :: user_powered2_rel(NDIM)  = [0.60_dp, 0.54_dp]
+  real(dp) :: user_grounded1_rel(NDIM) = [0.30_dp, 0.46_dp]
+  real(dp) :: user_grounded2_rel(NDIM) = [0.60_dp, 0.46_dp]
+
+  real(dp) :: powered1(NDIM), powered2(NDIM)
+  real(dp) :: grounded1(NDIM), grounded2(NDIM)
+
+  ! Small potential offset for the "grounded" electrode (Volts)
+  real(dp) :: grounded_phi_offset = 1.0e-3_dp
+
+  ! -------------------------
+  ! Dielectric slab
+  ! -------------------------
+  real(dp) :: dielectric_eps = 5.0_dp
+  real(dp) :: slab_y0 = 4.875e-3_dp
+  real(dp) :: slab_y1 = 8.125e-3_dp
 
 contains
 
@@ -29,67 +42,86 @@ contains
     type(af_t),  intent(inout) :: tree
 
     call CFG_add_get(cfg, "user_circle_radius", user_circle_radius, &
-         "Radius (m) of user-defined circle electrodes")
+         "Radius (m) of circular electrodes")
 
-    call CFG_add_get(cfg, "user_powered1_rel", user_powered1_rel, &
-         "Relative center (x,y) of powered circle 1")
-    call CFG_add_get(cfg, "user_powered2_rel", user_powered2_rel, &
-         "Relative center (x,y) of powered circle 2")
-    call CFG_add_get(cfg, "user_grounded1_rel", user_grounded1_rel, &
-         "Relative center (x,y) of grounded circle 1")
-    call CFG_add_get(cfg, "user_grounded2_rel", user_grounded2_rel, &
-         "Relative center (x,y) of grounded circle 2")
+    call CFG_add_get(cfg, "user_powered1_rel", user_powered1_rel, "Powered electrode 1 (rel)")
+    call CFG_add_get(cfg, "user_powered2_rel", user_powered2_rel, "Powered electrode 2 (rel)")
+    call CFG_add_get(cfg, "user_grounded1_rel", user_grounded1_rel, "Grounded electrode 1 (rel)")
+    call CFG_add_get(cfg, "user_grounded2_rel", user_grounded2_rel, "Grounded electrode 2 (rel)")
 
-    ! Convert relative -> absolute coordinates
+    call CFG_add_get(cfg, "grounded_phi_offset", grounded_phi_offset, &
+         "Small offset potential (V) applied to grounded electrode")
+
+    call CFG_add_get(cfg, "dielectric_eps", dielectric_eps, "Dielectric permittivity")
+    call CFG_add_get(cfg, "slab_y0", slab_y0, "Dielectric slab lower y (m)")
+    call CFG_add_get(cfg, "slab_y1", slab_y1, "Dielectric slab upper y (m)")
+
     powered1  = ST_domain_origin + user_powered1_rel  * ST_domain_len
     powered2  = ST_domain_origin + user_powered2_rel  * ST_domain_len
     grounded1 = ST_domain_origin + user_grounded1_rel * ST_domain_len
     grounded2 = ST_domain_origin + user_grounded2_rel * ST_domain_len
 
-    ! Register electrode callbacks
-    user_lsf    => my_user_lsf
-    user_lsf_bc => my_user_lsf_bc
+    user_lsf                => my_user_lsf
+    user_lsf_bc             => my_user_lsf_bc
+    user_initial_conditions => my_init_cond
   end subroutine user_initialize
 
-  ! Signed-distance-like function for a circle: ||x-c|| - radius
   pure real(dp) function circle_lsf(x, c, rad) result(lsf)
     real(dp), intent(in) :: x(NDIM), c(NDIM)
     real(dp), intent(in) :: rad
     lsf = norm2(x - c) - rad
   end function circle_lsf
 
-  ! Union of 4 circles -> min of individual lsf values
   real(dp) function my_user_lsf(x) result(lsf)
     real(dp), intent(in) :: x(NDIM)
-    real(dp) :: l1, l2, l3, l4
-
-    l1 = circle_lsf(x, powered1,  user_circle_radius)
-    l2 = circle_lsf(x, powered2,  user_circle_radius)
-    l3 = circle_lsf(x, grounded1, user_circle_radius)
-    l4 = circle_lsf(x, grounded2, user_circle_radius)
-
-    lsf = min(min(l1, l2), min(l3, l4))
+    lsf = min( &
+          min(circle_lsf(x, powered1,  user_circle_radius), &
+              circle_lsf(x, powered2,  user_circle_radius)), &
+          min(circle_lsf(x, grounded1, user_circle_radius), &
+              circle_lsf(x, grounded2, user_circle_radius)) )
   end function my_user_lsf
 
-  ! Boundary potential: powered circles -> current_voltage, grounded -> 0
   real(dp) function my_user_lsf_bc(x) result(phi)
     real(dp), intent(in) :: x(NDIM)
-    real(dp) :: l1, l2, l3, l4
-    real(dp) :: m_pow, m_gnd
+    real(dp) :: mp, mg
 
-    l1 = circle_lsf(x, powered1,  user_circle_radius)
-    l2 = circle_lsf(x, powered2,  user_circle_radius)
-    l3 = circle_lsf(x, grounded1, user_circle_radius)
-    l4 = circle_lsf(x, grounded2, user_circle_radius)
+    mp = min(circle_lsf(x, powered1, user_circle_radius), &
+             circle_lsf(x, powered2, user_circle_radius))
+    mg = min(circle_lsf(x, grounded1, user_circle_radius), &
+             circle_lsf(x, grounded2, user_circle_radius))
 
-    m_pow = min(l1, l2)
-    m_gnd = min(l3, l4)
-
-    if (m_pow < m_gnd) then
+    if (mp < mg) then
       phi = current_voltage
     else
-      phi = 0.0_dp
+      ! "Grounded" electrode with a tiny offset in Volts
+      phi = grounded_phi_offset
     end if
   end function my_user_lsf_bc
+
+subroutine my_init_cond(box)
+  type(box_t), intent(inout) :: box
+  real(dp) :: y_center
+  logical  :: in_slab
+
+  ! If dielectric not enabled (or eps not present), do nothing at all.
+  if (.not. ST_use_dielectric) return
+  if (i_eps <= 0) return
+
+  ! Box-centered y-coordinate
+  y_center = box%r_min(2) + 0.5_dp * box%dr(2) * box%n_cell
+  in_slab  = (y_center >= slab_y0 .and. y_center < slab_y1)
+
+  if (in_slab) then
+    ! Set dielectric permittivity
+    box%cc(:, :, i_eps) = dielectric_eps
+
+    ! Prevent plasma inside the dielectric ONLY
+    if (i_electron > 0) box%cc(:, :, i_electron) = 0.0_dp
+    if (i_1pos_ion  > 0) box%cc(:, :, i_1pos_ion) = 0.0_dp
+  else
+    box%cc(:, :, i_eps) = 1.0_dp
+  end if
+end subroutine my_init_cond
+
 
 end module m_user
